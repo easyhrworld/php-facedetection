90249d1be2b6903a8ffd5c525313bbc2ce05c374
Hiten Dedhia <hdedhia@streetlightsoftware.com>
2016-11-15 11:37:41 +0530

    Trying to fix code

--- FaceDetector.php
+++ FaceDetector.php
@@ -353,4 +353,37 @@ class FaceDetector
         }
         return true;
     }
+
+    public function Rotate()
+    {
+        $canvas = imagecreatetruecolor($this->face['w'], $this->face['w']);
+        imagecopy($canvas, $this->canvas, 0, 0, $this->face['x'],
+            $this->face['x'], $this->face['w'], $this->face['w']);
+        $canvas = imagerotate($canvas, 180, 0);
+        $this->_outImage($canvas);
+    }
+
+    public function toGrayScale()
+    {
+        $canvas = imagecreatetruecolor($this->face['w'], $this->face['w']);
+        imagecopy($canvas, $this->canvas, 0, 0, $this->face['x'],
+            $this->face['x'], $this->face['w'], $this->face['w']);
+        imagefilter($canvas, IMG_FILTER_GRAYSCALE);
+        $this->_outImage($canvas);
+    }
+
+    public function resizeFace($width, $height)
+    {
+        $canvas = imagecreatetruecolor($width, $width);
+        imagecopyresized($canvas, $this->canvas, 0, 0, $this->face['x'],
+            $this->face['y'], $width, $height,
+            $this->face['w'], $this->face['w']);
+        $this->_outImage($canvas);
+    }
+
+    private function _outImage($canvas)
+    {
+        header('Content-type: image/jpeg');
+        imagejpeg($canvas);
+    }
 }
